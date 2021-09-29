8c419af627aba1794cf4e0a82a08e8014aad7566
warband0 <55248186+warband0@users.noreply.github.com>
2021-09-29 06:19:36 +0300

    feat: add input

--- museum/js/range.js
+++ museum/js/range.js
@@ -4,3 +4,6 @@ progress.addEventListener('input', function() {
   const value = this.value;
   this.style.background = `linear-gradient(to right, #710707 0%, #710707 ${value}%, #C4C4C4 ${value}%, #C4C4C4 100%)`
 })
+
+
+console.log()
