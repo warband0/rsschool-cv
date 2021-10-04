54c8a309f359c3b408a0bc10a311b3c433f9624c
warband0 <55248186+warband0@users.noreply.github.com>
2021-10-05 02:33:59 +0300

    Delete museum directory

--- museum/js/rangevolume.js
+++
@@ -1,5 +0,0 @@
-const volume = document.querySelector('.volume');
-volume.addEventListener('input', function() {
-  const value = this.value;
-  this.style.background = `linear-gradient(to right, #710707 0%, #710707 ${value}%, #C4C4C4 ${value}%, #C4C4C4 100%)`
-})
