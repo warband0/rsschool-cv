ef1ed49d1ec7598adcefefaa86f8c3f7a872e0fa
warband0 <55248186+warband0@users.noreply.github.com>
2021-10-05 01:14:54 +0300

    feat: 1024 adaptive

---
+++ museum-adaptive/js/rangevolume.js
@@ -0,0 +1,5 @@
+const volume = document.querySelector('.volume');
+volume.addEventListener('input', function() {
+  const value = this.value;
+  this.style.background = `linear-gradient(to right, #710707 0%, #710707 ${value}%, #C4C4C4 ${value}%, #C4C4C4 100%)`
+})
